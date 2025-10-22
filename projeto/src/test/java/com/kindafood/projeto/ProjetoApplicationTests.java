c1e1c113febbf2b35d168de98b338c3b21be21d3
Caio Riciati <esdrascaio5313@gmail.com>
2025-10-21 22:31:35 -0300

    adicionando o projeto spring

---
+++ projeto/src/test/java/com/kindafood/projeto/ProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindafood.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
